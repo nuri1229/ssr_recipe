bda3022ded8f47da09f6ee873aa3e0a8a911c75c
nlee <nlee@energyx.co.kr>
2021-03-21 18:11:54 +0900

    add saga 이용한 detail 데이터 작업

--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -1,9 +1,18 @@
 import React from "react";
-import Users from "../components/Users";
 import UsersContainer from "../components/UsersContainer";
+import UserContainer from "../components/UserContainer";
+import { Route } from "react-router-dom";
 
 const UsersPage = () => {
-  return <UsersContainer />;
+  return (
+    <>
+      <UsersContainer />
+      <Route
+        path="/users/:id"
+        render={({ match }) => <UserContainer id={match.params.id} />}
+      />
+    </>
+  );
 };
 
 export default UsersPage;
